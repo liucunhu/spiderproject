0fbde67bf129f34a89a6d7695f52c2dd131ef529
liucunhu <786608954@qq.com>
2022-04-28 10:09:10 +0800

    Initial commit

--- midlePro/midlePro/settings.py
+++ midlePro/midlePro/settings.py
@@ -47,7 +47,7 @@ ROBOTSTXT_OBEY = False
 #SPIDER_MIDDLEWARES = {
 #    'midlePro.middlewares.MidleproSpiderMiddleware': 543,
 #}
-
+LOG_LEVEL="ERROR"
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 DOWNLOADER_MIDDLEWARES = {
